aeca761677c5c8be15cac5fe6ff775bde74afda0
Jerry <azhagirishankar@appstronauts.in>
2025-03-13 09:52:57 +0530

    #1 first commit

---
+++ resources/views/pages/dashboards/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master')
+
+@section('styles')
+  
+
+@endsection
+
+@section('content')
+
+            <!-- Start::app-content -->
+            <div class="main-content app-content">
+                <div class="container-fluid">
+
+                    <div class="row row-sm mt-lg-4">
+                        <div class="col-sm-12 col-lg-12 col-xl-12">
+                              <div class="card bg-primary rounded-sm">
+                                    <div class="card-body p-[20px]">
+                                          <span class="text-white">NOTE:</span>      
+                                          <p class="text-white mt-2 mb-0">Thank you for choosing our template. if you want to create your own customised project take a refrence of our project and you can implement here.</p>              
+                                    </div>
+                              </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <!-- End::app-content -->
+        
+@endsection
+
+@section('scripts')
+
+
+@endsection
---
+++ vite.config.js
@@ -0,0 +1,104 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import * as packages from './package.json';
+import fsExtra from 'fs-extra'; // Import fs-extra as a default import
+import { join } from 'path';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                // Resources paths 
+                'resources/css/app.css', 
+                'resources/sass/app.scss', 
+                'resources/js/app.js',
+
+                // Resources assets js file paths
+                'resources/assets/js/custom-switcher.js',
+                'resources/assets/js/defaultmenu.js',
+                'resources/assets/js/simplebar.js',
+            ],
+            refresh: true,
+        }),
+
+        viteStaticCopy({
+            targets: [
+                {
+                    src: ([
+                        'resources/assets/audio/', 
+                        'resources/assets/images/', 
+                        'resources/assets/icon-fonts/', 
+                        'resources/assets/video/', 
+                        
+                        'resources/assets/js/main.js', 
+                        'resources/assets/js/sticky.js', 
+                        'resources/assets/js/switch.js',
+                    ]),
+                    dest: 'assets/'
+                }
+            ]
+        }),
+
+        {
+            // Use a custom plugin for copying distribution files
+            name: 'copy-dist-files',
+            writeBundle: async () => {
+                const destDir = 'public/build/assets/libs';  // Update the destination directory
+      
+              for (const dep of Object.keys(packages.dependencies)) {
+                const srcPath = join('node_modules', dep, 'dist');
+                const destPath = join(destDir, dep);
+      
+                // Check if the 'dist' directory exists for the dependency
+                if (await fsExtra.pathExists(srcPath)) {
+                  // Copy the distribution files (contents of 'dist') to the destination directory
+                  await fsExtra.copy(srcPath, destPath, {
+                    overwrite: true,
+                    recursive: true,
+                  });
+      
+                  // Remove the 'dist' directory from the destination
+                  await fsExtra.remove(join(destPath, 'dist'));
+                } else {
+                  // If 'dist' folder doesn't exist, check if the package itself exists and copy it.
+                  const packageSrcPath = join('node_modules', dep);
+                  const packageDestPath = join(destDir, dep);
+      
+                  if (await fsExtra.pathExists(packageSrcPath)) {
+                    await fsExtra.copy(packageSrcPath, packageDestPath, {
+                      overwrite: true,
+                      recursive: true,
+                    });
+                  }
+                }
+              }
+            },
+        },
+
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith('.blade.php')) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    });
+                }
+            },
+        }
+    ],
+    css: {
+      preprocessorOptions: {
+          scss: {
+            api: 'modern-compiler',
+          },
+      },
+    },
+    build: {
+        chunkSizeWarningLimit: 1600,
+        outDir: 'public/build',
+        emptyOutDir: true,
+    },
+
+});
